7baacfc3889780091aae1814bf4df9e53b8a0183
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-25 18:35:32 +0300

    leave only declaration files inside git

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+import { ArrayType } from './types/ArrayType';
+import { Defined } from './types/Defined';
+import { Modify } from './types/Modify';
+import { NotRequired } from './types/NotRequired';
+import { PropsWith } from './types/PropsWith';
+import { PropsWithClassnames } from './types/PropsWithClassnames';
+import { PropsWithId } from './types/PropsWithId';
+import { PropsWithStyles } from './types/PropsWithStyles';
+export { ArrayType, Defined, Modify, NotRequired, PropsWith, PropsWithClassnames, PropsWithId, PropsWithStyles, };
---
+++ dist/types/PropsWithStyles.d.ts
@@ -0,0 +1,4 @@
+import { CSSProperties } from 'react';
+export type PropsWithStyles<P extends any> = P & {
+    style?: CSSProperties;
+};
