425145c1aff009cf529876000d2f012301ede547
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-26 22:44:08 +0300

    add type RecordKey

---
+++ src/types/RecordKey.ts
@@ -0,0 +1,14 @@
+/**
+ * Get typeof key of Record.
+ *
+ * @example
+ * type Key = RecordKey<Record<string, number>>; // string
+ */
+export type RecordKey<R extends Record<any, any>> = R extends Record<
+  infer K,
+  any
+>
+  ? K
+  : R;
+
+const str: RecordKey<Record<string, number>> = '';
