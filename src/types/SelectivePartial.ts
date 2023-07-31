09d6d55961c230d33d00fa932b7b8551a6e3605a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-31 23:48:21 +0300

    add type SelectivePartial

---
+++ src/types/SelectivePartial.ts
@@ -0,0 +1,17 @@
+/**
+ * Works as Partial, but makes only specified keys
+ * partial.
+ *
+ * @example
+ * type Super = SelectivePartial<{
+ *  name: string;
+ *  address: {
+ *    street: string;
+ *    number: number
+ *  }
+ * }, 'address'>;
+ *
+ * // Property 'address' will be partial.
+ */
+export type SelectivePartial<T, K extends keyof T> = Omit<T, K> &
+  Partial<Pick<T, K>>;
