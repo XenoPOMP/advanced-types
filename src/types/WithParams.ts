c26371c476f9c0db5b6337c8695d6b2cd491a139
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-11-07 17:32:05 +0300

    add additional undefined check in NextParams

--- src/types/WithParams.ts
+++ src/types/WithParams.ts
@@ -1,11 +1,21 @@
 /**
  * This type describes Next parameters.
  */
-export type NextParams<K extends string> = Record<K, string>;
+export type NextParams<K extends string> = K extends undefined
+  ? unknown
+  : Record<K, string>;
 
 /**
  * Adds property *__params__* of type {@link NextParams} with
  * defined parameter names (second generic argument. Is optional).
+ *
+ * @example
+ * type Props = WithParams<{ args?: Array<number> }>;
+ *
+ * // unknown
+ * const parameters: Props['params'] = {
+ *   ...
+ * };
  *
  * @example
  * type Props = WithParams<{ args?: Array<number> }, 'id'>;
