87aa4b8cb82db7d1a59c209696a056ea79d76779
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-01-28 10:53:56 +0300

    Delete test.ts

--- src/test.ts
+++
@@ -1,5 +0,0 @@
-import { WithParams } from './types/WithParams';
-import { WithSearchParams } from './types/WithSearchParams';
-
-type Sus = WithParams<{}, 'serviceId' | 'name'>['params'];
-type Amongus = WithSearchParams<{}, 'serviceId' | 'name'>['searchParams'];
