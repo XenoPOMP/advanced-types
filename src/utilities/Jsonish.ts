87f3b8270ec701b5f1b7b8193e99ebc327b639c9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-22 21:32:31 +0300

    🧹 update Jsonish type

--- src/utilities/Jsonish.ts
+++ src/utilities/Jsonish.ts
@@ -1,14 +1,4 @@
-import { RecursiveKeyValuePair } from 'tailwindcss/types/config';
-import { Sus } from 'type-fest';
-
-import { Primitive } from '../aliases';
-
-import { Nullable } from './Nullable';
-
-type JsonSchemaType = RecursiveKeyValuePair<
-  PropertyKey,
-  Nullable<Primitive> | Array<Nullable<Primitive>>
->;
+import { Jsonifiable } from 'type-fest';
 
 /**
  * Return never if type does not match JSON schema.
@@ -17,6 +7,4 @@ type JsonSchemaType = RecursiveKeyValuePair<
  * type Schema = Jsonish<{ sus: string; am: string; }>; // OK
  * type AlterSchema = Jsonish<{ sus: string; am: () => {}; }>; // Causes error
  */
-export type Jsonish<T extends JsonSchemaType> = T extends JsonSchemaType
-  ? T
-  : never;
+export type Jsonish<T extends Jsonifiable> = T extends Jsonifiable ? T : never;
