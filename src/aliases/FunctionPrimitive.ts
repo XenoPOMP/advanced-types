1d89b0542551492ec0049bea1bcc99c549c57a09
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-22 21:25:44 +0300

    🫥 create deprecation module

--- src/aliases/FunctionPrimitive.ts
+++ src/aliases/FunctionPrimitive.ts
@@ -1,5 +1,9 @@
+import { Fn } from '../deprecation';
+
 /**
  * This type is function primitive that you can use as type `extends` constraint.
+ *
+ * @deprecated Use {@link Fn} instead
  *
  * @example
  * type QueryConstant<
