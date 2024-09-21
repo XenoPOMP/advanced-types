aab8125ebe0334495831083db9f403f420477762
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-09-21 15:53:13 +0300

    🐛 finish OneOf

--- src/utilities/OneOf.ts
+++ src/utilities/OneOf.ts
@@ -1,9 +1,17 @@
+import { MergeTypes } from './MergeTypes';
+
 /**
  * Extracts first type from two generics. Is helper type for {@link OneOf} type.
  */
 export type OnlyFirst<F, S> = F & { [Key in keyof Omit<S, keyof F>]?: never };
 
 /**
- * @TODO
+ * Chooses only one of types array.
  */
-export type OneOf = unknown;
+export type OneOf<
+  TypesArray extends any[],
+  Res = never,
+  AllProperties = MergeTypes<TypesArray>
+> = TypesArray extends [infer Head, ...infer Rem]
+  ? OneOf<Rem, Res | OnlyFirst<Head, AllProperties>, AllProperties>
+  : Res;
