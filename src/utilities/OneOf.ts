2cb5f787f80265ad142d3264dec312f7d5d2b5d1
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-09-21 15:35:32 +0300

    📐 create OnlyFirst

--- src/utilities/OneOf.ts
+++ src/utilities/OneOf.ts
@@ -1,4 +1,9 @@
 /**
+ * Extracts first type from two generics. Is helper type for {@link OneOf} type.
+ */
+export type OnlyFirst<F, S> = F & { [Key in keyof Omit<S, keyof F>]?: never };
+
+/**
  * @TODO
  */
 export type OneOf = unknown;
