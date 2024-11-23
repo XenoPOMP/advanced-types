b102d834f97b817149ce0f3cbcc4d149a7056f57
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-11-23 11:05:14 +0200

    📝 update docs

--- src/react/FcProps.ts
+++ src/react/FcProps.ts
@@ -2,5 +2,12 @@ import { FC } from 'react';
 
 /**
  * Extracts type of props from FC type.
+ *
+ * @example
+ * type Props = FcProps<FC<{ align?: boolean }>>;
+ * //   ^? { align?: boolean }
+ *
+ * type VariableProps = FcProps<VariableFC<'button', { notAlign?: boolean }>>;
+ * //   ^? {notAlign?: boolean} & Omit<ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement>, undefined>
  */
 export type FcProps<Comp> = Comp extends FC<infer Props> ? Props : never;
